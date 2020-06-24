81025672a3a049841a9f12d5d268148e72af57ae
Marcin Wojtkowski <marcinwojtkowski94@gmail.com>
2020-06-23 22:05:50 -0700

    Completed Stage 5 and most of Stage 6. Creation of dict and storing done. Added in a check for the existence of data.json and a deletion so that I can receive updated information from googles API

--- main.py
+++ main.py
@@ -27,13 +27,25 @@
     Stage 5
         Create a dict with business names and their business status as a key_value
         pair and store them locally.
+            #successfully created the dictionary
     Stage 6
         Check the key value pairs against themselves for a future feature update
+        ~UP~DATE~
+        Store the key value pair of business names and business status.
+            #done
+        Rerun the program and check if business status has changed.
+            -this requires reruning the API call and creation of a new file to
+            re-parse through.
+            #success
+        If business status has changed, create a log file to document the change.
 
 '''
 
 import json
 import requests
+import os.path
+import os
+
 
 with open('api_key.txt', 'r') as f:
     api_key = f.readline()
@@ -44,15 +56,26 @@ find_place = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
              + '&keyword=hair%20cut'
              + '&key=' + api_key)
 
+'''
+if os.path.exists('data.json'):
+    os.remove('data.json')
 
-'''with requests.get(find_place) as response:
+with requests.get(find_place) as response:
     with open('data.json', 'w') as f:
         json.dump(response.json(), f, sort_keys=True, indent=4)
 '''
 
 with open('data.json', 'r') as f:
-    dict = json.load(f)
+    response_dict = json.load(f)
 
-for x in dict['results']:
-    print(json.dumps(x['name']))
-    print(json.dumps(x['business_status']))
+salon_status={}
+
+for x in response_dict['results']:
+    salon_status[x['name']] = x['business_status']
+    #print(json.dumps(x['name']))
+    #print(json.dumps(x['business_status']))
+
+if !os.path.exists('results.py'):
+    with open('results.py', 'w') as f:
+        for x in salon_status:
+            f.write(x + ' ---> ' + salon_status[x] + '\n')
